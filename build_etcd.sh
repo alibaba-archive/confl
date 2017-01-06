775af8c80188445af35995a296e80632949c4bc5
xushuai <dyxushuai@gmail.com>
2017-01-06 17:45:59 +0800

    travis ci support

---
+++ build_etcd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+VERSION="v3.0.15"
+FILE_ROOT="etcd-${VERSION}-linux-amd64"
+FILE_NAME="${FILE_ROOT}.tar.gz"
+
+curl -L "https://github.com/coreos/etcd/releases/download/${VERSION}/${FILE_NAME}" -o "${FILE_NAME}"
+tar xzvf "${FILE_NAME}"
+cd $FILE_ROOT
+
+echo "Starting etcd"
+./etcd &
+sleep 1
