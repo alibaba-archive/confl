708e6be2961ccb0c57075f5d200abc74087f4650
xu shuai <dyxushuai@gmail.com>
2017-01-11 20:01:33 +0800

    Update config.go

--- etcd/config.go
+++ etcd/config.go
@@ -1,6 +1,6 @@
 package etcd
 
-// Etcd configuration
+// Etcd configuration reviewdog test
 type Config struct {
 	Clusters []string `envconfig:"CONFL_ETCD_CLUSTERS" required:"true"`
 	Cert     string   `envconfig:"CONFL_ETCD_CERT"`
