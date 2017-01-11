4473431b7855aad542ecdc7ae57b37a5bc1917b4
xushuai <dyxushuai@gmail.com>
2017-01-11 13:17:23 +0800

    update example; more clear description

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -9,34 +9,37 @@ import (
 	"github.com/teambition/confl/vault"
 )
 
-// just for test
-func setEnv() {
+func main() {
+	// all supported enviroment variables see README
+	// perfect fit the docker, k8s and swarm etc
 	os.Clearenv()
 	os.Setenv("CONFL_CONF_PATH", "/confl/test")
 	os.Setenv("CONFL_ETCD_CLUSTERS", "http://localhost:2379")
 	os.Setenv("CONFL_VAULT_AUTH_TYPE", "token")
 	os.Setenv("CONFL_VAULT_ADDRESS", "http://localhost:8200")
 	os.Setenv("CONFL_VAULT_TOKEN", "06900225-b34b-69de-7872-21a2c8b52306")
-}
 
-type Config struct {
-	Username string        `json:"username"`
-	Password *vault.Secret `json:"password"`
-}
-
-func main() {
-	config := &Config{}
-	setEnv()
 	// set interval to 10 seconds just for test
 	// you need set it a little bigger in production
 	// perhaps DefaultInterval 5 minutes just ok
 	vault.DefaultInterval = 10 * time.Second
 
-	watcher, err := confl.NewFromEnv(config, nil)
+	// you configuration struct
+	// now just support json unmarshal
+	type Config struct {
+		Username string `json:"username"`
+		// *vault.Secret is a secure type for store password, secret, token etc
+		// it will load value from vault which is a tool for managing secrets
+		Password *vault.Secret `json:"password"`
+	}
+
+	watcher, err := confl.NewFromEnv(&Config{}, nil)
 	if err != nil {
 		panic(err)
 	}
 
+	// add hook for update events
+	// perhaps you need reload something that depends the configuration
 	watcher.AddHook(func(c interface{}) {
 		if cfg, ok := c.(Config); ok {
 			{
@@ -48,8 +51,10 @@ func main() {
 	})
 
 	// start watch
+	// it is a blocking method
 	go watcher.GoWatch()
 
+	// get configuration from watcher
 	cfg := watcher.Config().(Config)
 	{
 		// use cfg
